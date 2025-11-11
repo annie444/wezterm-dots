4ad10d848e3f63473264324ec8e2b20795c84bef
Annie Ehler <annie.ehler.4@gmail.com>
2025-11-10 16:59:39 -0800

    [2025-11-10 16:59:39] (update wezterm) Committing all changes to wezterm-dots

--- domains.lua
+++ domains.lua
@@ -7,18 +7,19 @@ M.apply_to_config = function(cfg)
     cfg.wezterm = require("wezterm")
   end
   local wezterm = cfg.wezterm
-  config.ssh_domains = wezterm.default_ssh_domains()
-  table.insert(config.ssh_domains, {
-    name = "spinoza",
-    remote_address = "spinoza.ipac.caltech.edu",
-    username = "annie",
-    multiplexing = "WezTerm",
-    ssh_option = {
-      identityfile = wezterm.home_dir .. "/.ssh/ipac_ed25519",
-      addkeystoagent = "yes",
-      forwardagent = "yes",
-      forwardx11 = "yes",
+  config.ssh_domains = {
+    {
+      name = "spinoza",
+      remote_address = "spinoza.ipac.caltech.edu",
+      username = "annie",
+      multiplexing = "WezTerm",
+      ssh_option = {
+        identityfile = wezterm.home_dir .. "/.ssh/ipac_ed25519",
+        addkeystoagent = "yes",
+        forwardagent = "yes",
+        forwardx11 = "yes",
+      },
     },
-  })
+  }
 end
 return M
