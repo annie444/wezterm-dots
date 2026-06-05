ec84ce64c83fe0742a2958da71215267b0178a01
Analetta "Annie" Ehler <annie.ehler.4@gmail.com>
2026-06-05 12:58:25 -0700

    [2026-06-05 12:58:24] (update wezterm) Committing all changes to wezterm-dots

--- domains.lua
+++ domains.lua
@@ -5,21 +5,30 @@ M.apply_to_config = function(cfg)
   local config = cfg.config
   if cfg.wezterm == nil then cfg.wezterm = require("wezterm") end
   local wezterm = cfg.wezterm
-  config.ssh_domains = {
-    {
-      name = "spinoza.ipac.caltech.edu",
-      remote_address = "spinoza.ipac.caltech.edu",
-      username = "annie",
-      multiplexing = "WezTerm",
-      ssh_option = {
-        identityfile = wezterm.home_dir .. "/.ssh/ipac_ed25519",
-        addkeystoagent = "yes",
-        forwardagent = "yes",
-        forwardx11 = "yes",
-        setenv = "PINENTRY_USER_DATA=curses",
-      },
+  config.ssh_domains = wezterm.default_ssh_domains()
+  ---@type SshDomain
+  local spinoza_domain = {
+    name = "spinoza",
+    remote_address = "spinoza.ipac.caltech.edu",
+    no_agent_auth = false,
+    username = "annie",
+    multiplexing = "WezTerm",
+    local_echo_threshold_ms = 10,
+    ssh_option = {
+      port = 22,
+      user = "annie",
+      identityfile = wezterm.home_dir .. "/.ssh/ipac_ed25519",
+      addkeystoagent = "yes",
+      forwardagent = "yes",
+      forwardx11 = "yes",
+      setenv = "PINENTRY_USER_DATA=curses",
+      serveraliveinterval = 60,
+      serveralivecountmax = 240,
+      hashknownhosts = "no",
+      userknownhostsfile = wezterm.home_dir .. "/.ssh/known_hosts",
     },
   }
+  table.insert(config.ssh_domains, spinoza_domain)
   config.default_workspace = "main"
 end
 return M
